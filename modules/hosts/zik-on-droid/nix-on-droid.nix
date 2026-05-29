b8eef8d67c92b05e1123fd9e5ae29780ca2d06a7
Zik <zik121311@outlook.com>
2026-05-29 19:01:28 +0300

    шыш

--- modules/hosts/zik-on-droid/nix-on-droid.nix
+++ modules/hosts/zik-on-droid/nix-on-droid.nix
@@ -11,7 +11,8 @@ imports = [
   # Simply install just the packages
   environment.packages = with pkgs; [
     # User-facing stuff that you really really want to have
-    neovim # or some other editor, e.g. nano or neovim
+    # neovim # or some other editor, e.g. nano or neovim
+    nano
     inputs.nixGL.packages.aarch64-linux.default
     mesa.drivers
     vulkan-tools
@@ -22,30 +23,32 @@ imports = [
     vulkan-validation-layers
     vulkan-extension-layer
     shaderc
+    openssh
     zsh-nix-shell
   ];
+  terminal.sshd.enable = true;
 
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
   environment.etcBackupExtension = ".bak";
 
   # Read the changelog before changing this value
-  system.stateVersion = "24.05";
+  # system.stateVersion = "24.05";
 
   # Set up nix for flakes
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
 
-  # Set your time zone
-  time.timeZone = "Europe/Moscow";
+  # # Set your time zone
+  # time.timeZone = "Europe/Moscow";
 
-  # Configure home-manager
-  home-manager = {
-    backupFileExtension = "hm-bak";
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    imports = [ self.homeModules.zik-on-droid ];
-  };
+  # # Configure home-manager
+  # home-manager = {
+  #   backupFileExtension = "hm-bak";
+  #   useGlobalPkgs = true;
+  #   useUserPackages = true;
+  #   imports = [ self.homeModules.zik-on-droid ];
+  # };
 
 };
 flake.homeModules.zik-on-droid = { config, lib, pkgs, ... }:
